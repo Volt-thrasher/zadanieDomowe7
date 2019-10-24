c3fe6117b70269b821f868af767995eda0a06783
Volt <sonianadashkevich@gmail.com>
2019-10-24 22:31:27 +0200

    zadanie1 poprawione zadanie2 poprawione

--- src/numberSum/Number.java
+++ src/numberSum/Number.java
@@ -4,7 +4,7 @@ public class Number {
 
     public static int numberSum(int num) {
         int sum = 0;
-        for (int i = 1; num / Math.pow(10, i) >= 0.1; i++) {
+        for (int i = 1; num / Math.pow(10, i) > 0; i++) {
             sum += (num % Math.pow(10, i) / Math.pow(10, i - 1));
         }
         return sum;
