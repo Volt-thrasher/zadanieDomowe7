510f6eca414713eb8ccba9c894a1348f5e076167
Volt <sonianadashkevich@gmail.com>
2019-10-24 22:44:32 +0200

    zadanie1 poprawione zadanie2 poprawione

--- src/numberSum/Number.java
+++ src/numberSum/Number.java
@@ -4,8 +4,12 @@ public class Number {
 
     public static int numberSum(int num) {
         int sum = 0;
-        for (int i = 1; num / Math.pow(10, i) > 0; i++) {
-            sum += (num % Math.pow(10, i) / Math.pow(10, i - 1));
+        for (int i = 1; num / Math.pow(10, i) > 0; i++)
+        {
+            sum += (num % Math.pow(10, i) / Math.pow(10, i - 1)); //do sumy dodaje reszte od dzielenia liczby
+                                            // przez 10^i, ktora dzielimy przez potege 10 o 1 mniejsza
+                                            // zeby odciac cala koncowke. W wyniku zostaje jedna cyfra okreslajaca
+                                            // liczbe danej potegi 10 
         }
         return sum;
     }
