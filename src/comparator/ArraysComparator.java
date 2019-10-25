ca38d2eef2c5ee99e9082a23014fa88e074ab8dc
Volt <sonianadashkevich@gmail.com>
2019-10-25 15:46:13 +0200

    zadanie1 poprawione zadanie2 poprawione zadanie4

--- src/comparator/ArraysComparator.java
+++ src/comparator/ArraysComparator.java
@@ -15,4 +15,24 @@ public class ArraysComparator {
         }else result= false;
         return result;
     }
+    public static boolean compare(int[][] array1, int[][]array2){
+        boolean result = false;
+        if (array1.length==array2.length){
+            for (int i = 0; i<array1.length; i++){
+                if (array1[i]==array2[i]) {
+                    if (array1[i].length==array2[i].length){
+                    for (int j = 0; j<array1[i].length; j++){
+                        if (array1[i][j]==array2[i][j]) {
+                            result = true;
+                        }else {
+                            result = false;
+                            break;
+                        }
+                        }
+                    }
+                }
+            }
+        }else result=false;
+        return result;
+    }
 }
--- src/comparator/ArraysTest.java
+++ src/comparator/ArraysTest.java
@@ -6,5 +6,13 @@ public class ArraysTest {
         int[] array1 = {1, 2, 3, 5, 7, 2, 8};
         int[] array2 = {1, 2, 3, 5, 7, 2};
         System.out.println(ArraysComparator.compare(array1,array2));
+
+        int[][] array11 = new int[2][3];
+        int[][] array22 = new int[2][3];
+        array11[0]= new int[]{1, 2, 3};
+        array11[1]= new int[]{3,4,6};
+        array22[0]= new int[]{1, 2, 3};
+        array22[1]= new int[]{3,4,6};
+        System.out.println(ArraysComparator.compare(array11,array22));
     }
 }
