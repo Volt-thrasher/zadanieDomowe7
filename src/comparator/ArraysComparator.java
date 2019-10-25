53fad5abc17380635542bc1ef987c96bb5d5573c
Volt <sonianadashkevich@gmail.com>
2019-10-25 15:53:01 +0200

    zadanie1 poprawione zadanie2 poprawione zadanie4 dokonczone

--- src/comparator/ArraysComparator.java
+++ src/comparator/ArraysComparator.java
@@ -1,38 +1,38 @@
 package comparator;
 
 public class ArraysComparator {
-    public static boolean compare(int[] array1, int[] array2){
+    public static boolean compare(int[] array1, int[] array2) {
         boolean result = false;
-        if(array1.length==array2.length){
-            for (int i=0; i<array1.length; i++){
-                if (array1[i]==array2[i]) {
-                    result= true;
-                }else {
+        if (array1.length == array2.length) {
+            for (int i = 0; i < array1.length; i++) {
+                if (array1[i] == array2[i]) {
+                    result = true;
+                } else {
                     result = false;
                     break;
                 }
             }
-        }else result= false;
+        } else result = false;
         return result;
     }
-    public static boolean compare(int[][] array1, int[][]array2){
+
+    public static boolean compare(int[][] array1, int[][] array2) {
         boolean result = false;
-        if (array1.length==array2.length){
-            for (int i = 0; i<array1.length; i++){
-                if (array1[i]==array2[i]) {
-                    if (array1[i].length==array2[i].length){
-                    for (int j = 0; j<array1[i].length; j++){
-                        if (array1[i][j]==array2[i][j]) {
-                            result = true;
-                        }else {
-                            result = false;
-                            break;
-                        }
+        if (array1.length == array2.length) {
+            for (int i = 0; i < array1.length; i++) {
+                    if (array1[i].length == array2[i].length) {
+                        for (int j = 0; j < array1[i].length; j++) {
+                            if (array1[i][j] == array2[i][j]) {
+                                result = true;
+                            } else {
+                                result = false;
+                                break;
+                            }
                         }
                     }
-                }
             }
-        }else result=false;
+        } else result = false;
+
         return result;
     }
 }
